6609e4d5d8bff17b0feb63ba5bc70e82d688448a
TienDX <doxuantien96@gmail.com>
2024-10-09 08:37:12 +0700

    feat: add notification

--- apps/sgr-b2c-mobile/app/_layout.tsx
+++ apps/sgr-b2c-mobile/app/_layout.tsx
@@ -5,6 +5,15 @@ import 'react-native-reanimated';
 import { defaultThemeConfig, ThemeProvider } from 'shared-ui-components';
 import { Provider } from 'react-redux';
 import { store } from '@/src/redux/store';
+import * as Notifications from 'expo-notifications';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 export default function RootLayout() {
   // const [loaded] = useFonts({
